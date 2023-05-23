804c8032792839d0b754cc9e4aa748b80e65196b
Andy Langer <mightydodongo@gmail.com>
2023-05-22 21:24:00 -0500

    Express serve css javascript (#3)
    * CSS works
    
    * Javascript works

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import * as express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.static("assets"));
+app.use(express.static("client/dist"));
+
 app.get('/', (req, res) => {
     res.sendFile("homepage.html", {root: "./assets"});
 })
